239fe1549a2a15771db1ece9123e811c3ed3a69b
iurych <iury.vibe@gmail.com>
2024-04-25 11:32:54 -0300

    feat: config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreationAccountController } from './controller/create-account.controller'
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreationAccountController],
   providers: [PrismaService],
 })
