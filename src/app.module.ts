2bcaec378e9db76b67de1d3674e239343a52f70f
iurych <iury.vibe@gmail.com>
2024-04-30 22:53:49 -0300

    feat: add prisma to auth-controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controller/authenticate.controller'
 import { CreationAccountController } from './controller/create-account.controller'
+import { CreateQuestionController } from './controller/create-question.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
@@ -14,7 +15,11 @@ import { PrismaService } from './prisma/prisma.service'
     }),
     AuthModule,
   ],
-  controllers: [CreationAccountController, AuthenticateController],
+  controllers: [
+    CreationAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
--- src/controller/authenticate.controller.ts
+++ src/controller/authenticate.controller.ts
@@ -1,14 +1,53 @@
-import { Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+import { ZodValidationPipe } from './pipes/zod-validation-pipe'
+
+const authBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type AuthRequestBody = z.infer<typeof authBodySchema>
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   @Post()
-  async handle() {
-    const token = this.jwt.sign({ sub: 'user-id' })
+  @UsePipes(new ZodValidationPipe(authBodySchema))
+  async handle(@Body() body: AuthRequestBody) {
+    const { email, password } = body
 
-    return token
+    const user = await this.prisma.user.findUniqueOrThrow({
+      where: { email },
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('User credentials were not found')
+    }
+
+    const validePass = await compare(password, user.password)
+
+    if (!validePass) {
+      throw new UnauthorizedException('Invalid credentials')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+
+    return {
+      access_token: accessToken,
+    }
   }
 }
